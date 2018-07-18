e06eb5554a976081ee38fa79bad915d304a918aa
Roberto Zazo Lopez <roberto@MacBook-Pro-de-Roberto.local>
2018-07-18 11:06:14 +0200

    initial commit

---
+++ src/bar.js
@@ -0,0 +1,8 @@
+import foo from "./foo";
+
+const message = () => foo("Hello");
+
+const meaning = 42;
+const day = "Wednesday";
+
+export { meaning, day, message };
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import foo from "./foo";
+import { meaning, day, message } from "./bar";
+
+foo("Zazo!");
+foo("Lazaro!");
+foo("Its a webpack!");
+foo(meaning);
+foo(day);
+message();
